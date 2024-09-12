59478a55a2257bc1af2e90bead013769af34b80b
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-09-12 16:15:48 +0200

    read file from cmd args, and add full invaders rom

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::env;
+
 mod disassembler;
 mod io_spainem;
 
 fn main() {
-    let op_buffer = io_spainem::read_from_file("invaders.h");
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() == 2, "No file provided");
+    let op_buffer = io_spainem::read_from_file(&args[1]);
     // let op_buffer_test: Vec<u8> = vec![0x11, 0x23, 0x32];
     let num_bytes = u16::try_from(op_buffer.len()).expect("Binary file is too big.");
     let mut pc: u16 = 0;
